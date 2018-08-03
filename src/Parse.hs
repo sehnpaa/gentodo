239b3976ec22f610d50aeb691086f035ae04045f
Peter Andersson <sehnpaa@gmail.com>
2018-08-03 15:27:56 +0200

    Create modules Parse and Types

---
+++ src/Parse.hs
@@ -0,0 +1,88 @@
+module Parse where
+
+import Control.Monad (void)
+import Data.Char (isDigit)
+import qualified Data.Text as T
+import Data.Time.Calendar (diffDays, fromGregorian)
+import Safe (maximumMay)
+import qualified Text.Parsec as P
+import qualified Text.Parsec.Text as PT
+
+import Types
+
+parseErrorToText :: P.ParseError -> T.Text
+parseErrorToText = T.pack . show
+
+process :: Date -> T.Text -> T.Text -> Either P.ParseError [Output]
+process currentDate logContent ambitionsContent = do
+    logs <- parseLogEntries logContent
+    ambitions <- parse ambitionsContent
+    return $ getOutputs currentDate logs ambitions
+
+parse :: T.Text-> Either P.ParseError [Ambition]
+parse = P.parse parseAmbitions ""
+
+parseAmbitions :: PT.Parser [Ambition]
+parseAmbitions = P.endBy parseAmbition eol
+
+eol :: PT.Parser String
+eol = P.string "\n"
+
+parseAmbition :: PT.Parser Ambition
+parseAmbition = do
+    n <- P.satisfy isDigit
+    void $ P.char ';'
+    desc <- parseDescription
+    return $ Ambition (Frequency (read [n]) Day) desc
+
+parseDescription :: PT.Parser T.Text
+parseDescription = do
+    desc <- P.many $ P.noneOf "\n"
+    return (T.pack desc)
+
+parseLogEntries :: T.Text -> Either P.ParseError [LogEntry]
+parseLogEntries = P.parse parseLogEntries2 ""
+
+parseLogEntries2 :: PT.Parser [LogEntry]
+parseLogEntries2 = P.endBy parseLogEntry eol
+
+parseLogEntry :: PT.Parser LogEntry
+parseLogEntry = do
+    date <- parseDate
+    void $ P.char ';'
+    desc <- parseDescription
+    return $ LogEntry date desc
+
+parseDate :: PT.Parser Date
+parseDate = do
+    year <- parseNumber
+    void $ P.char '-'
+    month <- parseNumber
+    void $ P.char '-'
+    day <- parseNumber
+    return $ fromGregorian (read year) (read month) (read day)
+
+parseNumber :: PT.Parser String
+parseNumber = P.many P.digit
+
+getOutputs :: Date -> [LogEntry] -> [Ambition] -> [Output]
+getOutputs currentDate logEntries =
+    map description . filter (relevantAmb currentDate logEntries)
+
+relevantAmb :: Date -> [LogEntry] -> Ambition -> Bool
+relevantAmb date logEntries a =
+    case getLatestLogEntry logEntries a of
+        Nothing -> True
+        (Just dateFoundInLog) ->
+            diffDays date dateFoundInLog >= num (frequency a)
+
+getLatestLogEntry :: [LogEntry] -> Ambition -> Maybe Date
+getLatestLogEntry logEntries a =
+    getLatestDate $ filter (hasSameDescription a) logEntries
+
+hasSameDescription :: Ambition -> LogEntry -> Bool
+hasSameDescription a (LogEntry _ logDesc) = logDesc == description a
+
+getLatestDate :: [LogEntry] -> Maybe Date
+getLatestDate logEntries = maximumMay $ map logDate logEntries
+
---
+++ src/Types.hs
@@ -0,0 +1,24 @@
+module Types where
+
+import qualified Data.Text as T
+import qualified Data.Time.Calendar as Cal
+
+data DWMY = Day | Week | Month | Year deriving Show
+
+data Frequency = Frequency
+  { num :: Integer
+  , unit :: DWMY } deriving Show
+
+data Ambition = Ambition
+  { frequency :: Frequency
+  , description :: T.Text } deriving Show
+
+data LogEntry = LogEntry
+  { logDate :: Date
+  , logDescription :: T.Text } deriving Show
+
+type Output = T.Text
+
+type Todo = T.Text
+
+type Date = Cal.Day
