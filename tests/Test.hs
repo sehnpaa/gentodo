2d42c607d6c4366ab62254fcc27ebdffa4b61189
Peter Andersson <sehnpaa@gmail.com>
2019-03-11 13:35:31 +0100

    Make sure 'process' does the right thing

--- tests/Test.hs
+++ tests/Test.hs
@@ -9,6 +9,7 @@ import Test.Tasty.HUnit
 import Lib
   ( getFormatedEntries
   , getTodos )
+import Parse (process)
 
 main :: IO ()
 main = defaultMain tests
@@ -32,5 +33,21 @@ unitTests = testGroup "Unit tests"
   , testCase "getTodos" $
       let actual = getTodos ["foo", "bar"]
           expected = ["\nfoo", "\nbar"]
+      in actual @?= expected
+  , testCase "process - activity should be added" $
+      let actual = process date20190307 "2019-03-06;activity1\n" "1;activity1\n"
+          expected = Right ["activity1"]
+      in actual @?= expected
+  , testCase "process - same day" $
+      let actual = process date20190307 "2019-03-07;activity1\n" "1;activity1\n"
+          expected = Right []
+      in actual @?= expected
+  , testCase "process - two ambitions" $
+      let actual = process date20190307 "2019-03-06;activity1\n2019-03-07;activity2\n" "1;activity1\n1;activity2\n"
+          expected = Right ["activity1"]
+      in actual @?= expected
+  , testCase "process - two ambitions - less frequent" $
+      let actual = process date20190307 "2019-03-06;activity1\n2019-03-05;activity2\n" "2;activity1\n2;activity2\n"
+          expected = Right ["activity2"]
       in actual @?= expected
   ]
